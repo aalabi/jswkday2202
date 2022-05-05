5879ceeee06796b6bd6c5f61afb70b5b0ef6bb21
Alabi A <alabi10@yahoo.com>
2022-05-05 08:55:39 +0100

    started class 3

---
+++ class3-array/script.js
@@ -0,0 +1,16 @@
+let keeper = "";
+let centralDefender = "";
+
+let team = ["de g", "harry m", "elanga", ""];
+
+console.log(team[0]);
+
+let urName = "Alabi A.";
+let age = 46;
+let isMarried = true;
+let dateOfDeath = undefined;
+
+let me = ["Alabi A", 46, true];
+me[5] = null;
+console.log(`My name is ${me[0]} I am ${me[1]} old`);
+console.log(me);
