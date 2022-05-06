593425c335ea3826d54ef413a901f7eec1ce94e0
Alabi A <alabi10@yahoo.com>
2022-05-06 13:39:07 +0100

    finished array lession

--- class3-array/script.js
+++ class3-array/script.js
@@ -12,5 +12,22 @@ let dateOfDeath = undefined;
 
 let me = ["Alabi A", 46, true];
 me[5] = null;
-console.log(`My name is ${me[0]} I am ${me[1]} old`);
-console.log(me);
+/* console.log(`My name is ${me[0]} I am ${me[1]} old`);
+console.log(me); */
+
+/* const officeOfYarAdua = ["governor", "president"];
+officeOfYarAdua[0] = "sug president";
+console.log(officeOfYarAdua);
+ */
+
+let cars = [];
+cars[3] = "Camry";
+console.log(cars);
+
+const exam = [
+  ["Who is the President of Nigeria?", "Alabi", "Adebayo", "Kolade", "Alabi"],
+  ["Who is the President of USA", "Buhari", "Tunde", "Biden", "Biden"],
+  ["Who is the President of Ghana", "Kofo", "Guy", "Nana", "Nana"],
+  ["Who is the President of South Africa", "Kofo", "Buhari", "Cyril", "Cyril"],
+  ["Who is the President of UK", "Theresa", "Johnson", "Cyril", "Johnson"],
+];
