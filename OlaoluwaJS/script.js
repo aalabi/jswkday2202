77d10bc806f509e7abff16c5420cf23c3db114fb
Olaoluwa4glory <samtex0942@gmail.com>
2022-05-07 12:28:12 +0100

    JS first Assignment

---
+++ OlaoluwaJS/script.js
@@ -0,0 +1,16 @@
+let Urname = "Samuel Olaoluwa Akinyemi";
+let occupation = "Digital Creator";
+let town = "No 16, kunleshow street, Ayobo, Ipaja Lagos,";
+let age = "32 years old.";
+
+
+// let sentence =
+//   "My name is " + Urname + 
+//   ", I am a " + occupation + 
+//   ", <br></br> I live at " + town + 
+//   " and I am " + age;
+
+sentence = `<div class ="style1">My name is ${Urname}, I am a ${occupation}, <br></br> I live in ${town} <br></br> and I am ${age}</div> `;
+
+console.log(sentence);
+document.write(sentence);
