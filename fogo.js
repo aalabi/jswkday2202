64bb9be50a9ad13cecbe42f2476ae213d711a873
Fogo Adepitan <Fogoadepitan100@gmail.com>
2022-05-08 12:25:26 +0100

    I added the java script about myself

---
+++ fogo.js
@@ -0,0 +1,13 @@
+const name="Adepitan Fogo";
+let age;
+const isMarried = true;
+let occupation;
+age=20;
+occupation="web dev";
+let bankBalance = 1000;
+let town="ikeja";
+
+
+let sentence = "My name is " + name +",I am a " +occupation + " and I live in "+ town;
+console.log(sentence);
+document.write(sentence);
