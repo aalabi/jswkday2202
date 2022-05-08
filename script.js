8ed873d2607e67adefd6190160af5b6081b8df57
segunmotunde <sogunledun@gmail.com>
2022-05-09 00:47:58 +0100

    first

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.write("Hello ");
+const name = "Segun";
+let character = ["Web Developer", "Bariga", 23];
+
+sentence = `my name is ${name}, I am a ${character[0]}. I live at ${character[1]} and I am ${character[2]} years old`;
+
+document.write(sentence);
