52b7c3931e61187bfdd5cd91022fb9078b81872f
Theophilus Enyichukwu <philuxds@gmail.com>
2022-05-09 01:34:07 +0100

    exam questions and answers

---
+++ assignment/script.js
@@ -0,0 +1,12 @@
+
+const examQuestions = [
+    ["who is the president of America ?", "Joe Biden", "bill clinton", "George Bush", "Joe Biden"]
+    [ "who is the prime minister of UK ?", "boris johnson", "queen Elizbeth", "tony bleir", "boris johnson"]
+    ["who is the governor  of lagos state ?", "babatunde sowolu", "ahmed tinubu", "olusola Ambode", "babatunde sowolu"]
+    ["who is the president of Russia ?", "valdimir putin", "john brown", "charles tailor", "valdimir putin"]
+    ["who is the australian president ?", "scot morrison", "ben bruce", "mark bill", "scot morrison"]
+
+
+
+
+]
