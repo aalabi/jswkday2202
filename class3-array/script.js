e0c320d4d1d0beaa184db6a17964c9270a91598e
ogheneare ochuko <oogheneare@gmail.com>
2022-05-08 22:05:12 -0700

    added questions and answer to class3-array

--- class3-array/script.js
+++ class3-array/script.js
@@ -31,3 +31,24 @@ const exam = [
   ["Who is the President of South Africa", "Kofo", "Buhari", "Cyril", "Cyril"],
   ["Who is the President of UK", "Theresa", "Johnson", "Cyril", "Johnson"],
 ];
+
+let question = "";
+
+question +=`
+
+1). ${exam[0][0]}<br>
+(a) ${exam[0][1]} &nbsp;(b) ${exam[0][2]}&nbsp;(c) ${exam[0][3]}<br><br>
+
+2). ${exam[1][0]}<br>
+(a) ${exam[1][1]} &nbsp;(b) ${exam[1][2]} &nbsp;(c) ${exam[1][3]}<br><br>
+
+3). ${exam[2][0]}<br>
+(a) ${exam[2][1]} &nbsp;(b) ${exam[2][2]} &nbsp;(c) ${exam[2][3]}<br><br>
+
+4). ${exam[3][0]}<br>
+(a) ${exam[3][1]} &nbsp;(b) ${exam[3][2]} &nbsp;(c) ${exam[3][3]}<br><br>
+
+5). ${exam[4][0]}<br>
+(a) ${exam[4][1]} &nbsp;(b) ${exam[4][2]} &nbsp;(c) ${exam[4][3]}<br><br>
+`
+  document.write(question);
