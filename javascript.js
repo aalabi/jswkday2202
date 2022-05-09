a937385dc3fb2806b305695eac523ee43549a9ed
segunmotunde <sogunledun@gmail.com>
2022-05-09 01:40:29 +0100

    added the question on repeat

--- javascript.js
+++ javascript.js
@@ -5,13 +5,23 @@ const exam = [
     ["Who is the President of South Africa", "Cyril", "Yemisi", "John", "Cyril"],
     ["Who is the President of UK", "Johnson", "Timi", "Olumide", "Johnson"],
 ];
+let question = "";
+let noOfQuestion = exam.length;
+let count = 0;
 
+while (count < noOfQuestion){
+let increment = count + 1;
 
-question = `
+question += `
     1). ${exam[0][0]} <br/>
     (a) ${exam[0][1]}  (b) ${exam[0][2]} (c) ${exam[0][3]}
     <br/><br/>
-`
+`;
+++count;
+}
 
 
+
+
+document.write(noOfQuestion);
 document.write(question);
