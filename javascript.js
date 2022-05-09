7a0167ea65047585f719ebbe5510e10bc2242f19
segunmotunde <sogunledun@gmail.com>
2022-05-09 01:16:11 +0100

    add the question

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-document.write("Hi");
+const exam = [
+    ["Who is the President of Nigeria", "Segun", "Buhari", "Olamide", "Buhari"],
+    ["Who is the President of USA?", "Biden", "Fashola", "Kunle", "Biden"],
+    ["Who is the President of Ghana", "Mide", "Tomiwa", "Nana", "Nana"],
+    ["Who is the President of South Africa", "Cyril", "Yemisi", "John", "Cyril"],
+    ["Who is the President of UK", "Johnson", "Timi", "Olumide", "Johnson"],
+];
+
+
