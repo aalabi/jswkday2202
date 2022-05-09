b6f826eac449468caa0d7aba788f2f648df0e2c7
Alabi A <alabi10@yahoo.com>
2022-05-09 11:30:52 +0100

    explain loop concept

--- class5-loop/script.js
+++ class5-loop/script.js
@@ -1,14 +1,26 @@
-let question = "";
-let noOfQuestions = exam.length;
-let kanter = 0;
-while (kanter < noOfQuestions) {
-  let questionNo = kanter + 1;
-  question += `
-    ${questionNo}). ${exam[kanter][0]}<br/>
-    (a) ${exam[kanter][1]} (b) ${exam[kanter][2]} (c) ${exam[kanter][3]}
-    <br/><br/>
-`;
-  ++kanter;
+{
+  document.write("Hello World!<br/>");
+  document.write("Hello World!<br/>");
+  document.write("Hello World!<br/>");
+  document.write("Hello World!<br/>");
+  document.write("Hello World!<br/>");
+  document.write("Hello World!<br/>");
+  document.write("Hello World!<br/>");
+  document.write("Hello World!<br/>");
 }
 
-document.write(question);
+document.write("<br/> <br/> <br/>");
+let kanter = 0;
+/* while (kanter < 12) {
+  ++kanter;
+  document.write(kanter + " X 2 = " + kanter * 2 + "<br/>");
+} */
+
+/* do {
+  ++kanter;
+  document.write(kanter + " X 2 = " + kanter * 2 + "<br/>");
+} while (kanter < 12); */
+
+for (let index = 2; index <= 12; index = index + 2) {
+  document.write(index + " X 2 = " + index * 2 + "<br/>");
+}
