21320cb15028b62ace8773b178eef17a29bfa2d3
Otite Akpofure <akpofureotite111@gmail.com>
2022-05-11 18:08:03 +0100

    commented out former assignement and updated the new one using even and odd numbers

--- otis/otis.js
+++ otis/otis.js
@@ -19,46 +19,46 @@
 // document.write("examAnswer");
 
 
-const exam = [
-    ["Who is the president of Nigeria?", " Muhammadu Buhari", "James Cameron" , "Kelvin James", "Muhammadu Buhari"],
-    ["Who is the president of Liberia?", "Helen Justin Sirleaf", "Philemon Joshua", "Pinto Nutjis" , "Helen Justin Sirleaf"],
-    ["Who is the president of Congo?", "Newman Jake", "fred Likto", "Kelvin Bingo" , "Newman Jake"],
-    ["Who is the president of South Afirca?", "kelvin tayong", "Odin thor", "Striden Joshua" , "kelvin tayong"],
-    ["Who is the president of Zambia?", "Uhuru David", "Stephen Olidin", "Fredrick Freepong" , "Botin Jimmy"],
-];
+// const exam = [
+//     ["Who is the president of Nigeria?", " Muhammadu Buhari", "James Cameron" , "Kelvin James", "Muhammadu Buhari"],
+//     ["Who is the president of Liberia?", "Helen Justin Sirleaf", "Philemon Joshua", "Pinto Nutjis" , "Helen Justin Sirleaf"],
+//     ["Who is the president of Congo?", "Newman Jake", "fred Likto", "Kelvin Bingo" , "Newman Jake"],
+//     ["Who is the president of South Afirca?", "kelvin tayong", "Odin thor", "Striden Joshua" , "kelvin tayong"],
+//     ["Who is the president of Zambia?", "Uhuru David", "Stephen Olidin", "Fredrick Freepong" , "Botin Jimmy"],
+// ];
 
-const examAnswer= `
-   <table width = 1200 border =1>
+// const examAnswer= `
+//    <table width = 1200 border =1>
 
-   <tr>
-  <td> ${1.}</td><td colspan=2> ${exam[0][0]}</td> <td>${"a."} ${exam[0][1]}</td> <td>${"b."}  ${exam[0][2]}</td> <td> ${"c."} ${exam[0][3]}</td> 
-   </tr>
+//    <tr>
+//   <td> ${1.}</td><td colspan=2> ${exam[0][0]}</td> <td>${"a."} ${exam[0][1]}</td> <td>${"b."}  ${exam[0][2]}</td> <td> ${"c."} ${exam[0][3]}</td> 
+//    </tr>
 
-   <tr> 
-   <td>${2.}</td> <td colspan=2 > ${ exam[1][0]} </td>  <td>${"a."}${exam[1][1]} </td>  <td>${"b."}${exam[1][2]} </td> <td>${"c."} ${exam[1][3]}</td> 
-   </tr>
+//    <tr> 
+//    <td>${2.}</td> <td colspan=2 > ${ exam[1][0]} </td>  <td>${"a."}${exam[1][1]} </td>  <td>${"b."}${exam[1][2]} </td> <td>${"c."} ${exam[1][3]}</td> 
+//    </tr>
    
-   <tr>
-   <td>${3.}</td>  <td colspan=2> ${ exam[2][0]} </td> <td>${"a."} ${exam[2][1]}</td> <td>${"b."} ${exam[2][2]} </td> <td>${"c."} ${exam[2][3]}</td>
-   </tr> 
+//    <tr>
+//    <td>${3.}</td>  <td colspan=2> ${ exam[2][0]} </td> <td>${"a."} ${exam[2][1]}</td> <td>${"b."} ${exam[2][2]} </td> <td>${"c."} ${exam[2][3]}</td>
+//    </tr> 
 
-   <tr>
-   <td> ${4.}</td>  <td colspan=2>${ exam[3][0]} </td> <td> ${"a."} ${exam[3][1]}</td>  <td> ${"b."} &nbsp${exam[3][2]}</td> <td> ${"c."} ${exam[3][3]} </td> 
+//    <tr>
+//    <td> ${4.}</td>  <td colspan=2>${ exam[3][0]} </td> <td> ${"a."} ${exam[3][1]}</td>  <td> ${"b."} &nbsp${exam[3][2]}</td> <td> ${"c."} ${exam[3][3]} </td> 
 
-   </tr>
+//    </tr>
    
-   <td>${5.}</td> <td  colspan=2 >${ exam[4][0]}</td> <td>${"a."} ${exam[4][1]} </td> <td>${"b."}${exam[4][2]} </td> <td>${"c."} ${exam[4][3]} </td>
-   </table>
-`;
+//    <td>${5.}</td> <td  colspan=2 >${ exam[4][0]}</td> <td>${"a."} ${exam[4][1]} </td> <td>${"b."}${exam[4][2]} </td> <td>${"c."} ${exam[4][3]} </td>
+//    </table>
+// `;
 
-document.write(examAnswer);
+// document.write(examAnswer);
 
-//calculation for multiplaication table 
+// //calculation for multiplaication table 
 
-for( i=1; i<12; i++){
+// for( i=1; i<12; i++){
 
- document.write(` The answer is:${i} * 2 =  ${i*2} <br>` )
-}
+//  document.write(` The answer is:${i} * 2 =  ${i*2} <br>` )
+// }
 
 // for( let i=0; i<12; i++){
 //     console.log( i + " x 2" + i*2)
@@ -67,10 +67,10 @@ for( i=1; i<12; i++){
 // this is the second assignment 
 
 
-let examsQuestions = exam.filter(exams =>{
-return exam[0]*2
-})
-console.log(examsQuestions);
+// let examsQuestions = exam.filter(exams =>{
+// return exam[0]*2
+// })
+// console.log(examsQuestions);
 
 
 const exam = [
